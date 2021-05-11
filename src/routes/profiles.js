e1f3c8d4ece92050845db0aa13a5b4ad87a58e5d
Ardi Germenji <67866525+a-pyro@users.noreply.github.com>
2021-05-11 12:19:32 +0200

    hello brah

--- src/routes/profiles.js
+++ src/routes/profiles.js
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import { Router } from 'express';
 import {
   createExperience,
   deleteExperience,
@@ -7,7 +7,7 @@ import {
   getUserExperiences,
   modifyExperience,
   uploadExperiencePic,
-} from "../controllers/experiences.js";
+} from '../controllers/experiences.js';
 import {
   createProfile,
   getProfile,
@@ -15,38 +15,40 @@ import {
   getProfiles,
   modifyProfile,
   uploadProfilePic,
-} from "../controllers/profiles.js";
+} from '../controllers/profiles.js';
 
 const router = Router();
 
-router.route("/").get(getProfiles).post(createProfile).put(modifyProfile);
+router.route('/').get(getProfiles).post(createProfile).put(modifyProfile);
 // GET POST AND UPDATE PROFILE
 
-router.route("/:userId").get(getProfile);
+router.route('/:profileId').get(getProfile);
 // GET 1 PROFILE WHICH MATCHES USERID
 
-router.route("/:userId/picture").post(uploadProfilePic);
+router.route('/:profileId/picture').post(uploadProfilePic);
 // Uploads a profile photo
 
-router.route("/:userId/CV").get(getProfilePdfCV);
+router.route('/:profileId/CV').get(getProfilePdfCV);
 //Generates and download a PDF with the CV of the user (details, picture, experiences)
 
 router
-  .route("/userName/experiences")
+  .route('/:profileId/experiences')
   .get(getUserExperiences)
   .post(createExperience);
 // GET POST AND UPDATE PROFILE
 
 router
-  .route("/userName/experiences/:expId")
+  .route('/:profileId/experiences/:expId')
   .get(getSingleExperience)
   .put(modifyExperience)
   .delete(deleteExperience);
 
-router.route("/userName/experiences/:expId/picture").post(uploadExperiencePic);
+router
+  .route('/:profileId/experiences/:expId/picture')
+  .post(uploadExperiencePic);
 // Uploads an experience photo
 
-router.route("/userName/experiences/CSV").get(getExperciencesCSV);
+router.route('/:profileId/experiences/CSV').get(getExperciencesCSV);
 //Download user experiences as a CSV file
 
 export default router;

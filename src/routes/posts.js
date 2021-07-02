c738be72a11a6adb1b823e87b2e256795518893c
Ardi Germenji <67866525+a-pyro@users.noreply.github.com>
2021-07-02 13:12:56 +0200

    fixes comments routes

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,24 +1,36 @@
-import { Router } from "express";
+import { Router } from 'express';
 import {
+  createComment,
   createPost,
+  deleteComment,
   deletePost,
+  getComments,
   getPost,
   getPosts,
+  modifyComment,
   modifyPost,
   uploadPostPic,
-} from "../controllers/posts.js";
+} from '../controllers/posts.js';
 
 const router = Router();
 
-router.route("/").get(getPosts).post(createPost);
+router.route('/').get(getPosts).post(createPost);
 // GET POST AND CREATE POST
 
 router
-  .route("/:postId")
+  .route('/:postId')
   .get(getPost)
   .put(modifyPost)
   .delete(deletePost)
   .post(uploadPostPic);
 // GET singular Post // edit post // delete post // upload post picture
+
+router.route('/:postId/comments').get(getComments).post(createComment);
+
+router.route('/:postId/comments').post(createComment);
+
+router.route('/:postId/comment/:commentId').post(deleteComment);
+
+router.route('comments/:commentId').post(modifyComment);
 
 export default router;
